5cdf1b7d80ac7bba502e106205c191fb92f6475c
GaidukVD <115919792+GaidukVD@users.noreply.github.com>
2022-10-30 18:15:24 +0300

    Лабораторная работа 4

---
+++ task2.py
@@ -0,0 +1,30 @@
+def get_count_char(str_):
+    ...  # TODO посчитать количество каждой буквы в строке в аргументе str_
+    new_str = str_.lower()
+    new_str = "".join(new_str.split())
+    letter_dict = {}
+    DEFAULT_COUNT = 0
+    for i in new_str:
+        if i.isalpha():
+            letter_dict[i] = letter_dict.get(i, DEFAULT_COUNT) + 1
+    return letter_dict
+
+
+def get_count_proc(dict_):
+    sum = 0
+    letter_dict_proc = {}
+    for i in dict_:
+        sum += dict_.get(i)
+        # letter_dict_proc[i] = letter_dict_proc.get(i)
+    for n in dict_:
+        letter_dict_proc[n] = dict_.get(n)/sum
+    return letter_dict_proc
+
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
+main_dict = get_count_char(main_str)
+print(get_count_proc(main_dict))
